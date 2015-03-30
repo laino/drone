6d9524cac03360a35621b95ad5c454c2d5c48d83
laino <lain@volafile.io>
2015-03-30 11:42:28 +0200

    Nicer output

--- drone/control/control.c
+++ drone/control/control.c
@@ -10,7 +10,7 @@ int main(){
   }
   sensors_open();
   
-  if(sensors_select_accel_range(SENSORS_ACCEL_RANGE_16G)){
+  if(sensors_select_accel_range(SENSORS_ACCEL_RANGE_8G)){
     printf("Error selecting accel range");
     return 1;
   }
@@ -26,36 +26,39 @@ int main(){
 
   double temp;
 
+  printf("\033[2J");
+
   while(1){
 
     clock_gettime(CLOCK_MONOTONIC, &tstart);
 
-    sensors_read_accel_data(&accel, SENSORS_ACCEL_RANGE_16G);
+    sensors_read_accel_data(&accel, SENSORS_ACCEL_RANGE_8G);
     sensors_read_gyro_data(&gyro, SENSORS_GYRO_RANGE_250);
     sensors_read_temp_data(&temp);
 
     clock_gettime(CLOCK_MONOTONIC, &tend);
 
 
+    printf("\033[0;0H");
     printf("-----------------------------------------------------\n");
-    printf("ACCEL X: %6.3f Y: %6.3f Z: %6.3f TOTAL: %6.3f (g/s) \n",
+    printf("ACCEL X: %7.2f Y: %7.2f Z: %7.2f TOTAL: %7.2f (g/s)\n",
             accel.x, 
             accel.y, 
             accel.z, 
             sqrt(accel.x*accel.x + accel.y*accel.y + accel.z*accel.z));
 
-    printf("GYRO  X: %6.3f Y: %6.3f Z: %6.3f TOTAL: %6.3f (degrees/second)\n",
+    printf("GYRO  X: %7.1f Y: %7.1f Z: %7.1f TOTAL: %7.1f (degrees/second)\n",
             gyro.x, 
             gyro.y, 
             gyro.z, 
             gyro.x + gyro.y + gyro.z);
 
-    printf("TEMP   : %6.3f (C) TIME READING: %.5f (seconds)\n", 
+    printf("TEMP   : %7.2f (celsius)      TIME READING: %7.5f (seconds)\n", 
           temp,
           ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) - 
           ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
 
-    sleep(1);
+    usleep(100000);
   }
   
   sensors_close();
